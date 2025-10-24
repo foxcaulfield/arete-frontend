90778a99fcde609a5a760b3942c5cc1c92d4233a
foxcaulfield <fox.caulfield@gmail.com>
2025-10-24 23:18:32 +0200

    feat: add TextInput component for enhanced text input handling

---
+++ src/lib/components/TextInput.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { FullAutoFill } from "svelte/elements";
+	type withLabel = {
+		withLabel: string;
+		id: string;
+	};
+	type withoutLabel = {
+		withLabel?: undefined;
+		id?: string;
+	};
+	type TextInputProps = {
+		value?: string;
+		autocomplete?: FullAutoFill;
+		placeholder?: string;
+        nameIsSameAsId?: boolean;
+		[rest: string]: any;
+	} & (withLabel | withoutLabel);
+
+	// Mark `value` as bindable so parent components can use `bind:value={...}`
+	let { value = $bindable(), autocomplete, placeholder, name, withLabel, id, nameIsSameAsId, ...rest }: TextInputProps = $props();
+</script>
+
+{#if withLabel}
+	<label for={id}>{withLabel}</label>
+{/if}
+<input
+	bind:value
+	autocomplete={autocomplete ?? "off"}
+	type="text"
+	placeholder={placeholder ?? "Enter text"}
+	id={id}
+    name={nameIsSameAsId ? id : name} 
+	{...rest}
+/>
