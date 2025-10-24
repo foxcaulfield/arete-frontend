bdf643457df7f03422997a55010d10606705f0dd
foxcaulfield <fox.caulfield@gmail.com>
2025-10-24 23:18:13 +0200

    feat: add FormGroupTextInput and FormGroupTextarea components for improved input handling

---
+++ src/lib/components/FormGroupTextInput.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import TextInput from "./TextInput.svelte";
+
+	interface FormGroupTextInputProps {
+		cssClass?: string;
+		value: string;
+		placeholder?: string;
+		idName: string;
+		label?: string;
+		errorText?: string;
+		minMax: [number, number];
+		required?: boolean;
+		// [rest: string]: any;
+	}
+	const props: FormGroupTextInputProps = $props();
+</script>
+
+<div class={props.cssClass}>
+	<label for={props.idName}>{props.label}</label>
+	<TextInput
+		id={props.idName}
+		name={props.idName}
+		value={props.value}
+		placeholder={props.placeholder}
+		required={props.required}
+		minlength={props.minMax[0]}
+		maxlength={props.minMax[1]}
+		aria-invalid={props.errorText ? "true" : "false"}
+		aria-describedby={props.errorText ? `${props.idName}-error` : undefined}
+	/>
+	{#if props.errorText}
+		<span id={`${props.idName}-error`} class="field-error">{props.errorText}</span>
+	{/if}
+</div>
---
+++ src/lib/components/FormGroupTextarea.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	interface FormGroupTextareaProps {
+		cssClass?: string;
+		value: string;
+		errorText?: string;
+		idName: string;
+		label?: string;
+		placeholder?: string;
+		minMax: [number, number];
+        required?: boolean;
+		// [rest: string]: any;
+	}
+	const props: FormGroupTextareaProps = $props();
+</script>
+
+<div class={props.cssClass}>
+	<label for={props.idName}>{props.label}</label>
+	<textarea
+		id={props.idName}
+		name={props.idName}
+		value={props.value}
+		placeholder={props.placeholder}
+		required={props.required}
+		minlength={props.minMax[0]}
+		maxlength={props.minMax[1]}
+		aria-invalid={props.errorText ? "true" : "false"}
+		aria-describedby={props.errorText ? `${props.idName}-error` : undefined}
+	></textarea>
+	{#if props.errorText}
+		<span id={`${props.idName}-error`} class="field-error">{props.errorText}</span>
+	{/if}
+</div>
