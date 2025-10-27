80cce19ed7abe0c1bed4da1326bfbfd8b01f54a0
foxcaulfield <fox.caulfield@gmail.com>
2025-10-27 10:25:03 +0100

    refactor: streamline answer rendering logic in QuestionText component

--- src/lib/components/drill/QuestionText.svelte
+++ src/lib/components/drill/QuestionText.svelte
@@ -29,12 +29,34 @@
 
 <div class="question-text">
 	{#each parts as part, i (i)}
-		{#if !part.isAnswer}
-			<span>{part.text}</span>
-		{:else if isAnswered}
-			<span class="answer-revealed">{part.text}</span>
-		{:else}
-			<span class="answer-hidden">{part.text}</span>
-		{/if}
+		{@const shouldReveal = part.isAnswer && isAnswered}
+		{@const shouldHide = part.isAnswer && !isAnswered}
+		<span class:answer={true} class:answer-revealed={shouldReveal} class:answer-hidden={shouldHide}
+			>{part.text}</span
+		>
 	{/each}
 </div>
+
+<style>
+	.question-text {
+		font-size: 1.75rem;
+		font-weight: 600;
+		margin: 0.5rem 0 0.6rem;
+		color: var(--text);
+	}
+
+	.answer {
+		border-radius: var(--radius-sm);
+		padding: 0.1em 0em;
+		margin: 0 0.15em;
+	}
+
+	.answer-revealed {
+		color: var(--success);
+	}
+
+	.answer-hidden {
+		background-color: var(--accent);
+		color: var(--accent);
+	}
+</style>
