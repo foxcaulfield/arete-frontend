7cab1666650faea40bd41fb43c06a939b08821a9
foxcaulfield <fox.caulfield@gmail.com>
2025-11-01 23:01:02 +0100

    feat: integrate Tailwind CSS and Skeleton UI components
    - Added Tailwind CSS and Skeleton dependencies to package.json.
    - Created app.css to import Tailwind and Skeleton styles.
    - Updated +layout.svelte to use Skeleton's Navigation component for improved navigation structure.
    - Configured Vite to include Tailwind CSS plugin.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,29 +2,41 @@
 	import favicon from "$lib/assets/favicon.svg";
 	// import "@picocss/pico/css/pico.css";
 	import "$lib/styles/global.scss";
+	import "$lib/styles/app.css";
 	import { SvelteToast } from "@zerodevx/svelte-toast";
 	import type { LayoutProps } from "./$types";
-	
+	import { BikeIcon, BookIcon, HouseIcon, TreePalmIcon } from "@lucide/svelte";
+	import { Navigation } from "@skeletonlabs/skeleton-svelte";
+
 	const { children }: LayoutProps = $props();
-	
+
+	const links = [
+		{ href: "/", text: "Home", icon: HouseIcon },
+		{ href: "/profile", text: "Profile", icon: TreePalmIcon },
+		{ href: "/collections", text: "Collections", icon: BookIcon },
+		{ href: "/auth", text: "Auth", icon: BikeIcon },
+	];
+
+	let anchorBar = 'btn hover:preset-tonal flex-col items-center gap-1';
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav>
-	<ul></ul>
-	<ul>
-		<li><a href="/">Home</a></li>
-		<li><a href="/profile">Profile </a></li>
-		<li><a href="/collections">Collections</a></li>
-		<li><a href="/auth">Login (Register)</a></li>
-	</ul>
-</nav>
+<Navigation layout="bar">
+	<Navigation.Menu class="grid grid-cols-4 gap-2">
+		{#each links as link (link)}
+			{@const Icon = link.icon}
+			<a href={link.href} class={anchorBar}>
+				<Icon class="size-5" />
+				<span class="text-[10px]">{link.text}</span>
+			</a>
+		{/each}
+	</Navigation.Menu>
+</Navigation>
 
 <main>
-    <SvelteToast />
-    {@render children?.()}
+	<SvelteToast />
+	{@render children?.()}
 </main>
- 
